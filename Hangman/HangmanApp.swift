cbe2e784e8b1bce9df5c678b77097b46db27fac2
Bill <bill092804@gmail.com>
2025-09-12 10:07:02 +0800

    Initial Commit

---
+++ Hangman/HangmanApp.swift
@@ -0,0 +1,17 @@
+//
+//  HangmanApp.swift
+//  Hangman
+//
+//  Created by 陳芸萱 on 2025/9/12.
+//
+
+import SwiftUI
+
+@main
+struct HangmanApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
